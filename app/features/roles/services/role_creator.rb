9828a820ec017ee2b78878522b9fee5b552332f5
sjperalta <serpel.js@hotmail.com>
2023-06-20 20:15:34 -0600

    code refactor

--- app/features/roles/services/role_creator.rb
+++ app/features/roles/services/role_creator.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
-module Features
-  module Roles
-    module Services
-      # User creator class
-      class RoleCreator
-        def initialize(user_params={})
-          @user_params = user_params
-        end
+module Roles
+  module Services
+    # User creator class
+    class RoleCreator
+      def initialize(user_params={})
+        @user_params = user_params
       end
     end
   end
 end
+
