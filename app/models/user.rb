fe459d861968e28fbbc930785fc044a682753bee
Sergio J. Peralta <122115892+sjperalta@users.noreply.github.com>
2023-07-22 18:48:26 -0600

    Feature/gemfile refactoring (#18)
    * included needed gems for debugging and code formmating
    
    ---------
    
    Co-authored-by: sergio peralta <sergio.peralta@rhythmagency.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,30 +5,27 @@ class User < ApplicationRecord
   validates :roles, length: { maximum: 100 }
   validates :status, presence: true, numericality: { only_integer: true }
   validates :company_id, presence: false
-
   delegate :can?, :cannot?, to: :ability
+  devise :database_authenticatable, :registerable, :rememberable,
+  :omniauthable, omniauth_providers: [:google_oauth2]
+  alias_attribute :updated_at, :modified_at
 
   def ability
     @ability ||= Ability.new(self)
   end
-
-devise :database_authenticatable, :registerable, :rememberable,
-:omniauthable, omniauth_providers: [:google_oauth2]
-
-    alias_attribute :updated_at, :modified_at
-    private
-
-    def self.from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
   
-        user.id = auth.uid
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0,20]
-        user.full_name = auth.info.name
-        user.roles = "cliente"
-        user.status = 1
-        user.company_id = auth.uid
-        user.uid = auth.uid
-      end
+  private
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.id = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.full_name = auth.info.name
+      user.roles = "cliente"
+      user.status = 1
+      user.company_id = auth.uid
+      user.uid = auth.uid
     end
+  end
 end
