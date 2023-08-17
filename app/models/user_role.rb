52a5257c3c7ad35ec79544f20c5e334eee41ac35
Brad0788 <sanchezabj07@unitec.edu>
2023-08-16 21:37:10 -0600

    actualizado el swagger con agregar y listar users

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -2,4 +2,6 @@ class UserRole < ApplicationRecord
     acts_as_paranoid
     belongs_to :user, foreign_key: :userId
     belongs_to :role, foreign_key: :roleId
+
+    validates :userId, uniqueness: { scope: :roleId, message: 'User already has this role.' }
 end
